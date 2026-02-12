a09f1105832b84864f4ab6193cf1b7906c772e42
Dorah <momanyidora@gmail.com>
2026-02-12 09:37:22 +0300

    add this filr

--- getProperty.ts
+++ getProperty.ts
@@ -1,13 +1,13 @@
-function getProperty<T extends object, K extends keyof T>(obj:T, key:K){
-    return obj[key]
+// function getProperty<T extends object, K extends keyof T>(obj:T, key:K){
+//     return obj[key]
 
-}
-const person = {
-    name: "Reduzer",
-    age: 5
+// }
+// const person = {
+//     name: "Reduzer",
+//     age: 5
     
-}
-// const location = getProperty(person, "location")
-console.log(getProperty(person, "name"));
-console.log(getProperty(person, "age"));
-// console.log(getProperty(person, "location"))
+// }
+// // const location = getProperty(person, "location")
+// console.log(getProperty(person, "name"));
+// console.log(getProperty(person, "age"));
+// // console.log(getProperty(person, "location"))
