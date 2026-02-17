9b293b419d09af5d82b11110562633577892219b
Dorah <momanyidora@gmail.com>
2026-02-17 12:45:51 +0300

    3 files

--- oop.ts
+++
@@ -1,9 +0,0 @@
-
-
-function getFirstEl<T>(arr:T[]):T{
-    return arr[0]
-}
-const result1 = getFirstEl([1,2,3])
-console.log(result1)
-const result2 = getFirstEl(["a", "b", "c"])
-console.log(result2)
